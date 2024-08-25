917f6212cbbaa778944c8cd62401bf0d49ddbed9
Filipe Soccol <filipe.soccol@gmail.com>
2024-08-25 13:38:47 -0300

    Initialized templated for NextJS+Frog and Stackr Counter.

---
+++ README.md
@@ -0,0 +1,55 @@
+# DevDiner
+
+A project to serve a pooling application for collecting developers data related to their diets. This is important to track that data to serve different meal options during in-person events.
+
+The stack includes Web3Auth for wallet authentication, XMTP tools for users to sign their submissions on Frames and a Micro-rollup backend using Stackr.
+
+## Installation Front-end/Frames
+
+```sh
+npm install
+```
+
+## Installation Stackr Micro-rollups
+
+```sh
+curl -fsSL https://bun.sh/install | bash
+npm i -g @stackr/cli
+```
+
+Access stackr folder:
+
+```sh
+cd rollup
+cp .env.example .env
+```
+
+Fill the required informations inside the `.env` file according to your environment. You can fetch Vulkan RPC and Registry Address [here](https://docs.stf.xyz/build/references/providers-and-rpc/).
+
+For more informations on how to config your app please go [here](https://docs.stf.xyz/build/zero-to-one/build-your-first-mru).
+
+If everything is set and ready just run de micro-rollup using Bun:
+
+```
+bun start
+```
+
+## Running the app itself
+
+```sh
+npm run dev
+```
+
+## Running DevTools
+
+In another terminal window you should run
+
+```sh
+npx frog dev
+```
+
+Inside developer tool head to http://localhost:3000/api
+
+## Navigating the app
+
+To see the default page where you can register events please navigate to the default page http://localhost:3000.
---
+++ app/api/[[...routes]]/route.tsx
@@ -0,0 +1,62 @@
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog, TextInput } from 'frog'
+import { devtools } from 'frog/dev'
+// import { neynar } from 'frog/hubs'
+import { handle } from 'frog/next'
+import { serveStatic } from 'frog/serve-static'
+
+const app = new Frog({
+  assetsPath: '/',
+  basePath: '/api',
+  // Supply a Hub to enable frame verification.
+  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' }),
+  title: 'Frog Frame',
+})
+
+// Uncomment to use Edge Runtime
+// export const runtime = 'edge'
+
+app.frame('/', (c) => {
+  return c.res({
+    action: '/finish',
+    image: (
+      <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
+        Sign data
+      </div>
+    ),
+    intents: [
+      <Button.Signature target="/sign">Sign</Button.Signature>,
+    ]
+  })
+})
+
+app.frame('/finish', (c) => {
+  const { transactionId } = c
+  return c.res({
+    image: (
+      <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
+        Signature: {transactionId}
+      </div>
+    )
+  })
+})
+
+devtools(app, { serveStatic })
+
+export const GET = handle(app)
+export const POST = handle(app)
+
+// NOTE: That if you are using the devtools and enable Edge Runtime, you will need to copy the devtools
+// static assets to the public folder. You can do this by adding a script to your package.json:
+// ```json
+// {
+//   scripts: {
+//     "copy-static": "cp -r ./node_modules/frog/_lib/ui/.frog ./public/.frog"
+//   }
+// }
+// ```
+// Next, you'll want to set up the devtools to use the correct assets path:
+// ```ts
+// devtools(app, { assetsPath: '/.frog' })
+// ```
