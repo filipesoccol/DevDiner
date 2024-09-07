38a5c04cec9dc85e60ad6f07d168e0e4026d1338
Filipe Soccol <filipe.soccol@gmail.com>
2024-09-07 18:35:55 -0300

    Strip data from frame.

--- app/frame/[[...routes]]/route.tsx
+++ app/frame/[[...routes]]/route.tsx
@@ -38,40 +38,8 @@ app.frame('/:slug', async (c) => {
   return c.res({
     image: (
       <div style={{ background: '#F1E7DB', color: '#2C263D', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', fontSize: 20, padding: 10, height: '100%' }}>
-        <div style={{ display: 'flex' }}>{data.name}</div>
+        <div style={{ display: 'flex' }}>Join us at {data.name}</div>
         <div style={{ display: 'flex', fontSize: 10 }}>Participants: {data.participantCount}</div>
-        <div style={{ display: 'flex', flexDirection: 'row', width: '80%', justifyContent: 'flex-start', paddingTop: 5 }}>
-          <div style={{ display: 'flex', flexDirection: 'column', width: '10%', justifyContent: 'flex-start', alignItems: 'flex-end' }}>
-            {Object.entries(data.restrictionsSum).map(([restriction, count]) => (
-              <div style={{
-                display: 'flex',
-                fontSize: '10px',
-                height: '15px',
-                whiteSpace: 'nowrap',
-                textAlign: 'right',
-                borderRadius: '15px',
-                paddingRight: '20px'
-              }}>
-                {count ? ((count / data.participantCount) * 100).toFixed(1) : '0.00'}%
-              </div>
-            ))}
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'column', width: '90%', justifyContent: 'flex-start' }}>
-            {Object.entries(data.restrictionsSum).map(([restriction, count], index) => (
-              <div key={index} style={{
-                width: `${(count / data.participantCount) * 90}%`,
-                height: '10px',
-                fontSize: '10px',
-                backgroundColor: ['#40AABF', '#8AB34C', '#D6BF29', '#DB4439', '#F39C12', '#9B59B6', '#3498DB', '#2ECC71'][index % 8],
-                marginBottom: '5px',
-                display: 'flex',
-                alignItems: 'center',
-                borderRadius: '3px',
-                paddingLeft: '5px'
-              }}>{restriction}</div>
-            ))}
-          </div>
-        </div>
       </div>
     ),
     intents: [
