8aadfbef64391c59965fca7199309727154d6209
Filipe Soccol <filipe.soccol@gmail.com>
2024-09-07 18:31:23 -0300

    Trying reduce size of frame

--- app/frame/[[...routes]]/route.tsx
+++ app/frame/[[...routes]]/route.tsx
@@ -16,6 +16,10 @@ const app = new Frog<{ State: State }>({
   assetsPath: '/',
   basePath: '/frame',
   browserLocation: '/',
+  imageOptions: {
+    width: 320,
+    height: 240,
+  },
   // Supply a Hub to enable frame verification.
   hub: neynar({ apiKey: 'NEYNAR_FROG_FM' }),
   title: 'Dev Diner Frame',
@@ -33,16 +37,16 @@ app.frame('/:slug', async (c) => {
   const data = event.eventWithRestrictions;
   return c.res({
     image: (
-      <div style={{ background: '#F1E7DB', color: '#2C263D', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', fontSize: 40, padding: 40, height: '100%' }}>
+      <div style={{ background: '#F1E7DB', color: '#2C263D', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', fontSize: 20, padding: 10, height: '100%' }}>
         <div style={{ display: 'flex' }}>{data.name}</div>
-        <div style={{ display: 'flex', fontSize: 30 }}>Participants: {data.participantCount}</div>
-        <div style={{ display: 'flex', flexDirection: 'row', width: '90%', height: '400px', justifyContent: 'flex-start', paddingTop: 40 }}>
-          <div style={{ display: 'flex', flexDirection: 'column', width: '10%', height: '500px', justifyContent: 'flex-start', alignItems: 'flex-end' }}>
+        <div style={{ display: 'flex', fontSize: 10 }}>Participants: {data.participantCount}</div>
+        <div style={{ display: 'flex', flexDirection: 'row', width: '80%', justifyContent: 'flex-start', paddingTop: 5 }}>
+          <div style={{ display: 'flex', flexDirection: 'column', width: '10%', justifyContent: 'flex-start', alignItems: 'flex-end' }}>
             {Object.entries(data.restrictionsSum).map(([restriction, count]) => (
               <div style={{
                 display: 'flex',
-                fontSize: '30px',
-                height: '45px',
+                fontSize: '10px',
+                height: '15px',
                 whiteSpace: 'nowrap',
                 textAlign: 'right',
                 borderRadius: '15px',
@@ -52,18 +56,18 @@ app.frame('/:slug', async (c) => {
               </div>
             ))}
           </div>
-          <div style={{ display: 'flex', flexDirection: 'column', width: '90%', height: '500px', justifyContent: 'flex-start' }}>
+          <div style={{ display: 'flex', flexDirection: 'column', width: '90%', justifyContent: 'flex-start' }}>
             {Object.entries(data.restrictionsSum).map(([restriction, count], index) => (
               <div key={index} style={{
                 width: `${(count / data.participantCount) * 90}%`,
-                height: '40px',
-                fontSize: '30px',
+                height: '10px',
+                fontSize: '10px',
                 backgroundColor: ['#40AABF', '#8AB34C', '#D6BF29', '#DB4439', '#F39C12', '#9B59B6', '#3498DB', '#2ECC71'][index % 8],
                 marginBottom: '5px',
                 display: 'flex',
                 alignItems: 'center',
-                borderRadius: '10px',
-                paddingLeft: '10px'
+                borderRadius: '3px',
+                paddingLeft: '5px'
               }}>{restriction}</div>
             ))}
           </div>
